18f9c2342836ee9a3c49f747369f515374c03a38
Mahmoud-dev09 <mahmoud0114829@gmail.com>
2025-09-26 15:05:57 +0300

    Add constans + Create a file declartion for icons

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const explore = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-white">explore</Text>
+    </View>
+  );
+};
+
+export default explore;
